5fcb2aa6bc1caca2bebbc1a5a589a150fa5c3a24
dholy <holyfrancisdushime@gmail.com>
2023-05-10 08:39:47 +0200

    changement du contenu de n°5

--- main.py
+++ main.py
@@ -2,5 +2,10 @@
 
 #1. creation de la Liste 
 listes=['Marc','Jean','Holy','Mike','Jack','Mary','Gilles','Mathiew','Lise','Simon']
-print("---------liste de 10 éléments de type chaîne de caractère----------")
-print(listes)
+print("1.---------liste de 10 éléments de type chaîne de caractère----------\n")
+print(listes,"\n\n")
+
+#2 . changement du contenu de l'element n° 5
+listes[4]='Jane'
+print("2.---------changement du contenu de l'element n° 5-------------\n")
+print(listes,"\n\n")
