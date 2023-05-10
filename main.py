08ff0affe68e6f613c3633a3b55c0f59f9eed0f0
dholy <holyfrancisdushime@gmail.com>
2023-05-10 08:35:07 +0200

    creation d'une liste de 10 elements

---
+++ main.py
@@ -0,0 +1,6 @@
+# I. List of 10 Strings
+
+#1. creation de la Liste 
+listes=['Marc','Jean','Holy','Mike','Jack','Mary','Gilles','Mathiew','Lise','Simon']
+print("---------liste de 10 éléments de type chaîne de caractère----------")
+print(listes)
