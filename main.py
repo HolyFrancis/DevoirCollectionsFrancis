64ac54634f3e93fce4b7720a979330df23e817c2
dholy <holyfrancisdushime@gmail.com>
2023-05-10 08:44:22 +0200

    creation d'une list des elements contenant la lettre'a'

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 # I. List of 10 Strings
 
 #1. creation de la Liste 
-listes=['Marc','Jean','Holy','Mike','Jack','Mary','Gilles','Mathiew','Lise','Simon']
 print("1.---------liste de 10 éléments de type chaîne de caractère----------\n")
+listes=['Marc','Jean','Holy','Mike','Jack','Mary','Gilles','Mathiew','Lise','Simon']
+
 print(listes,"\n\n")
 
 #2 . changement du contenu de l'element n° 5
-listes[4]='Jane'
 print("2.---------changement du contenu de l'element n° 5-------------\n")
+listes[4]='Jane'
+
 print(listes,"\n\n")
+
+# 3. creation d'une list des elements contenant la lettre "a"
+print("3.--------creation d'une list des elements contenant la lettre 'a'---------\n")
+liste_copy=[]
+for list in listes:
+    if 'a' in list:
+        liste_copy.append(list)
+
+print(liste_copy)
